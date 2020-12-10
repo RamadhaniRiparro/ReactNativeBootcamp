53ce2cbd5656536fbcd580688c663270a8b71d12
RamadhaniRiparro <ramadhani9112@gmail.com>
2020-12-10 16:05:51 +0700

    first commit

---
+++ tugas-4-Function/Function.js
@@ -0,0 +1,31 @@
+/* Functions No 1 */
+function teriak() {
+    console.log("Halo Sanbers!");
+}
+   
+teriak();
+
+/* Functions No 2 */
+function kalikan(num1,num2) {
+    hasilKali = num1 * num2;
+    return hasilKali;
+}
+var num1 = 12
+var num2 = 4
+ 
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali) // 48
+
+/* Functions No 3 */
+
+
+
+var name = "Agus"
+var age = 30
+var address = "Jln. Malioboro, Yogyakarta"
+var hobby = "Gaming"
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan) 
+/*Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya 
+di Jln. Malioboro, Yogyakarta, dan saya punya hobby yaitu Gaming!" */
