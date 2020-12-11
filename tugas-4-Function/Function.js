acdc484d484fd29ca3ae5941223a31ded06d881c
RamadhaniRiparro <ramadhani9112@gmail.com>
2020-12-11 20:37:09 +0700

    first commit

--- tugas-4-Function/Function.js
+++ tugas-4-Function/Function.js
@@ -14,12 +14,12 @@ var num1 = 12
 var num2 = 4
  
 var hasilKali = kalikan(num1, num2)
-console.log(hasilKali) // 48
+console.log(hasilKali) 
 
 /* Functions No 3 */
 function introduce(name, age, address, hobby) {
     perkenalan = `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`;
-    return perkenalan
+    return perkenalan;
 }
 
 
@@ -29,6 +29,5 @@ var address = "Jln. Malioboro, Yogyakarta"
 var hobby = "Gaming"
  
 var perkenalan = introduce(name, age, address, hobby)
-console.log(perkenalan) 
-/*Menampilkan !"Nama saya Agus, umur saya 30 tahun, alamat saya 
-di Jln. Malioboro, Yogyakarta, dan saya punya hobby yaitu Gaming" */
+console.log(perkenalan) ;
+
---
+++ tugas-5-Array/Array.js
@@ -0,0 +1,8 @@
+/* Soal No. 1 (Range) */
+range(startNum, finishNum) {}
+
+console.log(range(1, 10)) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(1)) // -1
+console.log(range(11,18)) // [11, 12, 13, 14, 15, 16, 17, 18]
+console.log(range(54, 50)) // [54, 53, 52, 51, 50]
+console.log(range()) // -1 
